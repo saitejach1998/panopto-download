0c6c5a79f0672f45602f44f240ec667d77cb0db3
saitejach1998 <saiteja.ch1998@gmail.com>
2022-06-05 14:25:57 -0400

    Single and Multi Mode

--- hmmm.py
+++ hmmm.py
@@ -1,3 +1,4 @@
+from turtle import down
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,23 +7,42 @@ import time
 import os
 import wget
 import argparse
+from urllib.parse import urlparse
 
-def download_file(name,url,folder_name):
-    #print(url)
-    final_directory = os.path.join(os.getcwd(),folder_name)
-    if not os.path.exists(final_directory):
-        os.makedirs(final_directory)
 
+
+def process_page(url,folder_name=None):
+    i = url.replace("Viewer","Embed")
+    driver.get(i)
+    driver.add_cookie({"name":".ASPXAUTH","value":cookies[".ASPXAUTH"]})
+    driver.get(i)
+    
+    script = driver.find_elements_by_xpath('//script')[-1]
+    scstr = script.get_attribute("innerHTML")
+    d_url = scstr[scstr.find('"VideoUrl"')+12:scstr.find('"ViewerIconUrl"')-2]
+    furl = d_url.replace('\\',"")
+    if( "hls" in furl): 
+        furl = furl.replace('hls','mp4')
+        furl = furl[:furl.find("mp4")+3]
+    #print(furl)
+    if folder_name is not None:
+        download_file(driver.title,furl,folder_name)
+    else:
+        download_file(driver.title,furl)
+
+
+def download_file(name,url,folder_name=None):
     s = str(name).strip().replace(' ', '_')
     s = str(s).strip().replace('/', '-')
-    print(f"{name}")
-    wget.download(url,f"{final_directory}/{s}.mp4")
-    #r = requests.get(url,cookies=cookies,stream=True)
-    #with open(f"{s}.mp4",'wb+') as f:
-    #    for chunk in r.iter_content(chunk_size=1024*1024*32):
-    #        if chunk:
-    #            f.write(chunk)
-    #            f.flush()
+    print(f"Downloading {name} ...")
+    if folder_name is not None:
+        final_directory = os.path.join(os.getcwd(),folder_name)
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+        
+        wget.download(url,f"{final_directory}/{s}.mp4")
+    else:
+        wget.download(url,f"{s}.mp4")
     print(f" Done.")
 
 
@@ -34,6 +54,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # install chromedriver on home path
+    os.environ['WDM_LOG_LEVEL'] = '0'
     s=Service(ChromeDriverManager().install())
     if(args.bpath):
         options = webdriver.ChromeOptions()
@@ -50,27 +71,23 @@ if __name__ == "__main__":
         url = args.url
     if(args.auth):
         cookies[".ASPXAUTH"] = args.auth
-
-    driver.get(url)
-    driver.add_cookie({"name":".ASPXAUTH","value":cookies[".ASPXAUTH"]})
-    driver.get(url)
-    time.sleep(5)
-    players = driver.find_elements_by_xpath('//a[@class="thumbnail-link"]')
-    folder_name  = driver.find_elements_by_xpath('//*[@id="contentHeaderText"]')[0].get_attribute("innerHTML")
     
-    viewer_urls = [i.get_attribute('href') for i in players if i.get_attribute('href')]
-    embed_urls = [i.replace("Viewer","Embed") for i in viewer_urls]
-#driver.get(embed_urls[0])
-    for i in embed_urls:
-        driver.get(i)
-        script = driver.find_elements_by_xpath('//script')[-1]
-        scstr = script.get_attribute("innerHTML")
-        d_url = scstr[scstr.find('"VideoUrl"')+12:scstr.find('"ViewerIconUrl"')-2]
-        furl = d_url.replace('\\',"")
-        if( "hls" in furl): 
-            furl = furl.replace('hls','mp4')
-            furl = furl[:furl.find("mp4")+3]
-        #print(furl)
-        download_file(driver.title,furl,folder_name)
-
-
+    parsed_url = urlparse(url)
+    print(parsed_url.path)
+    if(parsed_url.path.endswith("/Pages/Viewer.aspx")):
+        print("Single video mode.")
+        process_page(url)
+        quit()
+    elif(parsed_url.path.endswith("/Sessions/List.aspx")):   
+        print("All videos mode.")    
+        driver.get(url)
+        driver.add_cookie({"name":".ASPXAUTH","value":cookies[".ASPXAUTH"]})
+        driver.get(url)
+        time.sleep(5)
+        players = driver.find_elements_by_xpath('//a[@class="thumbnail-link"]')
+        folder_name  = driver.find_elements_by_xpath('//*[@id="contentHeaderText"]')[0].get_attribute("innerHTML")
+        viewer_urls = [i.get_attribute('href') for i in players if i.get_attribute('href')]
+        [process_page(i,folder_name) for i in viewer_urls]
+        quit()
+    else:
+        print("Error parsing URL. Try adding/removing quotes from the URL parameter")
