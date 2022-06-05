94dc2f20acfd8276b6018b5a47a3e47fd5aadcac
saitejach1998 <saiteja.ch1998@gmail.com>
2022-06-05 00:28:54 -0400

    added browser path option

--- hmmm.py
+++ hmmm.py
@@ -3,15 +3,20 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
+import os
 import wget
 import argparse
 
-def download_file(name,url,cookies):
+def download_file(name,url,folder_name):
     #print(url)
-    s = str(name).strip().replace(' ', '')
+    final_directory = os.path.join(os.getcwd(),folder_name)
+    if not os.path.exists(final_directory):
+        os.makedirs(final_directory)
+
+    s = str(name).strip().replace(' ', '_')
     s = str(s).strip().replace('/', '-')
     print(f"{name}")
-    wget.download(url,f"{s}.mp4")
+    wget.download(url,f"{final_directory}/{s}.mp4")
     #r = requests.get(url,cookies=cookies,stream=True)
     #with open(f"{s}.mp4",'wb+') as f:
     #    for chunk in r.iter_content(chunk_size=1024*1024*32):
@@ -25,11 +30,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', type=str, required=True)
     parser.add_argument('--auth', type=str, required=True)
+    parser.add_argument('--bpath', type=str, required=True)
     args = parser.parse_args()
 
     # install chromedriver on home path
     s=Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+    if(args.bpath):
+        options = webdriver.ChromeOptions()
+        options.binary_location = args.bpath
+        driver = webdriver.Chrome(service=s,options=options)
+    else:
+        driver = webdriver.Chrome(service=s)
+    
     authn = ".ASPXAUTH"
     url = ""
     cookies = {".ASPXAUTH":""}
@@ -44,6 +56,8 @@ if __name__ == "__main__":
     driver.get(url)
     time.sleep(5)
     players = driver.find_elements_by_xpath('//a[@class="thumbnail-link"]')
+    folder_name  = driver.find_elements_by_xpath('//*[@id="contentHeaderText"]')[0].get_attribute("innerHTML")
+    
     viewer_urls = [i.get_attribute('href') for i in players if i.get_attribute('href')]
     embed_urls = [i.replace("Viewer","Embed") for i in viewer_urls]
 #driver.get(embed_urls[0])
@@ -57,6 +71,6 @@ if __name__ == "__main__":
             furl = furl.replace('hls','mp4')
             furl = furl[:furl.find("mp4")+3]
         #print(furl)
-        download_file(driver.title,furl,cookies)
+        download_file(driver.title,furl,folder_name)
 
 
